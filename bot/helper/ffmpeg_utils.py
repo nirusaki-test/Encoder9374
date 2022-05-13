2a83139152d7c86c76737f206a6f40bea1689a38
nirusaki-test <105138184+nirusaki-test@users.noreply.github.com>
2022-05-13 20:34:21 +0530

    Update ffmpeg_utils.py

--- bot/helper/ffmpeg_utils.py
+++ bot/helper/ffmpeg_utils.py
@@ -30,7 +30,8 @@ def encode(filepath):
       episode_no = new_name["episode_number"]
       joined_string = f"{joined_string}" + f" [Episode {episode_no}]"
     og = joined_string + " [@ANIXPO]" + ".mkv"
-    return og
+    strr = og
+    return strr
 
 def get_thumbnail(in_filename):
     out_filename = 'thumb1.jpg'
