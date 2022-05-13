cfb70eb8f25e7b60141fbef58331f689c4f5f8df
nirusaki-test <105138184+nirusaki-test@users.noreply.github.com>
2022-05-13 19:50:45 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -21,7 +21,7 @@ video_mimetype = [
 
 @app.on_message(filters.user(sudo_users) & filters.command(["start", "help"]))
 def help_message(app, message):
-    message.reply_text(f"Hi {message.from_user.mention()}\n**•I can encode Telegram files in x265/x264, just send me a video.**\n**•This Bot is Developed by @S136r136a1**\n**•Simple, Easy and Convenient to use**\n**Thanks**")
+    message.reply_text(f"Hi {message.from_user.mention()}\n**•I can Rename Telegram files And Send Sample (Especially Movies,Animes), just send me a video.**\n**•This Bot is Developed by @S136r136a1**\n**•Simple, Easy and Convenient to use**\n**Thanks**")
 
 @app.on_message(filters.user(sudo_users) & filters.incoming & (filters.video | filters.document))
 def encode_video(app, message):
@@ -29,7 +29,7 @@ def encode_video(app, message):
       if not message.document.mime_type in video_mimetype:
         message.reply_text("```Invalid Video !\nMake sure its a valid video file.```", quote=True)
         return
-    message.reply_text("```Added to queue...```", quote=True)
+    message.reply_text("Added To Rename", quote=True)
     data.append(message)
     if len(data) == 1:
      add_task(message)
