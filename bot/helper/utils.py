c92c94045d62bb436a849e4857de3c4737609f4a
nirusaki-test <105138184+nirusaki-test@users.noreply.github.com>
2022-05-16 16:29:59 +0530

    Update utils.py

--- bot/helper/utils.py
+++ bot/helper/utils.py
@@ -5,33 +5,33 @@ from pyrogram.types import Message
 from pyrogram.errors.exceptions.bad_request_400 import MessageNotModified
 from .ffmpeg_utils import encode, get_thumbnail, get_duration, get_width_height 
 
-def on_task_complete():
+async def on_task_complete():
     del data[0]
     if len(data) > 0:
       add_task(data[0])
 
-def add_task(message: Message):
+async def add_task(message: Message):
     try:
-      msg = message.reply_text("⬇️ **Downloading Video** ⬇️", quote=True)
-      filepath = message.download(file_name=download_dir)
-      msg.edit(f"Renaming The File")
-      og = encode(filepath)
+      msg = await message.reply_text("⬇️ **Downloading Video** ⬇️", quote=True)
+      filepath = await message.download(file_name=download_dir)
+      await msg.edit(f"Renaming The File")
+      og = await encode(filepath)
       if og:
         msg.edit("**⬆️ Starting To Upload**")
-        thumb = get_thumbnail(filepath)
-        width, height = get_width_height(filepath)
-        duration2 = get_duration(filepath)
-        msg.edit("**⬆️ Uploading Video ⬆️**")
-        app.send_video(video=filepath, chat_id=message.chat.id, supports_streaming=True, file_name=og, thumb=thumb, duration=duration2, width=width, height=height, caption=og)
+        thumb = await get_thumbnail(filepath)
+        width, height = await get_width_height(filepath)
+        duration2 = await get_duration(filepath)
+        await msg.edit("**⬆️ Uploading Video ⬆️**")
+        await app.send_video(video=filepath, chat_id=message.chat.id, supports_streaming=True, file_name=og, thumb=thumb, duration=duration2, width=width, height=height, caption=og)
         os.remove(filepath)
         os.remove(thumb)
-        msg.edit("**File Renamed")
-        msg.delete()
+        await msg.edit("**File Renamed")
+        await msg.delete()
       else:
-        msg.edit("**Error Contact @NIRUSAKIMARVALE**")
+        await msg.edit("**Error Contact @NIRUSAKIMARVALE**")
         os.remove(filepath)
     except MessageNotModified:
       pass
     except Exception as e:
-      msg.edit(f"```{e}```")
+      await msg.edit(f"```{e}```")
     on_task_complete()
