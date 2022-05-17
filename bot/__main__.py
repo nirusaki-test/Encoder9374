d6abbc5d6449639f9c6fbb9644046fe2249d5678
nirusaki-test <105138184+nirusaki-test@users.noreply.github.com>
2022-05-17 15:39:42 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -24,24 +24,24 @@ video_mimetype = [
 @app.on_message(filters.incoming & filters.command(["cmds", "cmd", "commands"]))
 async def help_message(app, message):
     if message.chat.id not in sudo_users:
-      return await message.reply_text("You Are Not Authorised To Use This Bot Contact @Nirusaki")
+      return await message.reply_text("**You Are Not Authorised To Use This Bot Contact @Nirusaki**")
     await message.reply_text(f"Hi {message.from_user.mention()}\n**•The List Of Commands Are As Follows -:**\n•```/start```**- To Start The Bot\n**•```/cmds```**-To Repeat This List**\n•**Maintained By @FIERCE_TOONS**")
 
 @app.on_message(filters.incoming & filters.command(["start", "help"]))
 async def help_message(app, message):
     if message.chat.id not in sudo_users:
-      return await message.reply_text("You Are Not Authorised To Use This Bot Contact @Nirusaki")
+      return await message.reply_text("**You Are Not Authorised To Use This Bot Contact @Nirusaki**")
     await message.reply_text(f"Hi {message.from_user.mention()}\n**•I can Encode Telegram files And Send Sample (Especially Movies,Animes), just send me a video.**\n**•This Bot is Developed by @NIRUSAKI_AYEDAEMON**\n**•Simple, Easy and Convenient to use**\n**Thanks**")
 
 @app.on_message(filters.incoming & (filters.video | filters.document))
 async def encode_video(app, message):
     if message.chat.id not in sudo_users:
-      return await message.reply_text("You Are Not Authorised To Use This Bot Contact @Nirusaki")
+      return await message.reply_text("**You Are Not Authorised To Use This Bot Contact @Nirusaki**")
     if message.document:
       if not message.document.mime_type in video_mimetype:
         await message.reply_text("**Send Any Video File**", quote=True)
         return
-    a = await message.reply_text("Added To Queue Please Wait...", quote=True)
+    a = await message.reply_text("**Added To Queue Please Wait...**", quote=True)
     data.append(message)
     if len(data) == 1:
      await add_task(message, a)
