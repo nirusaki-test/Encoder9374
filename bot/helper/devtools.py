86b70ab498a5ae7154a0fd460a160301f93257fe
nirusaki-test <105138184+nirusaki-test@users.noreply.github.com>
2022-05-17 20:15:40 +0530

    Update devtools.py

--- bot/helper/devtools.py
+++ bot/helper/devtools.py
@@ -51,3 +51,58 @@ async def exec_message_f(client, message):
             await message.reply_text(OUTPUT)
   else:
     return
+
+async def eval_message_f(client, message):
+    if message.from_user.id in AUTH_USERS:
+        status_message = await message.reply_text("Processing ...")
+        cmd = message.text.split(" ", maxsplit=1)[1]
+
+        reply_to_id = message.message_id
+        if message.reply_to_message:
+            reply_to_id = message.reply_to_message.message_id
+
+        old_stderr = sys.stderr
+        old_stdout = sys.stdout
+        redirected_output = sys.stdout = io.StringIO()
+        redirected_error = sys.stderr = io.StringIO()
+        stdout, stderr, exc = None, None, None
+
+        try:
+            await aexec(cmd, client, message)
+        except Exception:
+            exc = traceback.format_exc()
+
+        stdout = redirected_output.getvalue()
+        stderr = redirected_error.getvalue()
+        sys.stdout = old_stdout
+        sys.stderr = old_stderr
+
+        evaluation = ""
+        if exc:
+            evaluation = exc
+        elif stderr:
+            evaluation = stderr
+        elif stdout:
+            evaluation = stdout
+        else:
+            evaluation = "Success"
+
+        final_output = (
+            "**EVAL**: <code>{}</code>\n\n**OUTPUT**:\n<code>{}</code> \n".format(
+                cmd, evaluation.strip()
+            )
+        )
+
+        if len(final_output) > MAX_MESSAGE_LENGTH:
+            with open("eval.text", "w+", encoding="utf8") as out_file:
+                out_file.write(str(final_output))
+            await message.reply_document(
+                document="eval.text",
+                caption=cmd,
+                disable_notification=True,
+                reply_to_message_id=reply_to_id,
+            )
+            os.remove("eval.text")
+            await status_message.delete()
+        else:
+            await status_message.edit(final_output)
