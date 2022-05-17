85e96e93212f970fa7d67fdb2edc9580c48b4036
nirusaki-test <105138184+nirusaki-test@users.noreply.github.com>
2022-05-17 14:53:50 +0530

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -6,6 +6,7 @@ from dotenv import load_dotenv
 api_id = int(os.environ.get("API_ID"))
 api_hash = os.environ.get("API_HASH")
 bot_token = os.environ.get("BOT_TOKEN")
+ffmpeg = []
 download_dir = os.environ.get("DOWNLOAD_DIR", "downloads/")
 sudo_users = list(set(int(x) for x in os.environ.get("SUDO_USERS").split()))
 LOG_CHANNEL = os.environ.get("LOG_CHANNEL", "")
