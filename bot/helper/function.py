d92af5e639daf15d668a1bcdcbd6ce164b88b6cc
nirusaki-test <105138184+nirusaki-test@users.noreply.github.com>
2022-05-18 13:28:24 +0530

    Update function.py

--- bot/helper/function.py
+++ bot/helper/function.py
@@ -6,9 +6,9 @@ from bot import app, sudo_users, ffmpeg
 
 async def change_ffmpeg(app, message):
   try:
-  changeffmpeg = message.text.split(" ", maxsplit=1)[1]
-  ffmpeg.insert(0,changeffmpeg)
-  await app.reply_text("**Successfully Changed The FFMPEG-CODE**")
+    changeffmpeg = message.text.split(" ", maxsplit=1)[1]
+    ffmpeg.insert(0,changeffmpeg)
+    await app.reply_text(f"**Successfully Changed The FFMPEG-CODE To**\n```{changeffmpeg}```")
   except Exception as e:
     await app.reply_test(f"Error {e}")
   
