dd354e700b8054a53a45a3b8f49925a7371b22af
nirusaki-test <105138184+nirusaki-test@users.noreply.github.com>
2022-05-18 13:17:16 +0530

    Update function.py

--- bot/helper/function.py
+++ bot/helper/function.py
@@ -1,1 +1,14 @@
+import pyrogram
+import asyncio
+import time
+import subprocess
+from bot import app, sudo_users, ffmpeg
 
+async def change_ffmpeg(app, message):
+  try:
+  changeffmpeg = message.text.split(" ", maxsplit=1)[1]
+  ffmpeg.insert(0,changeffmpeg)
+  await app.reply_text("**Successfully Changed The FFMPEG-CODE**")
+  except Exception as e:
+    await app.reply_test(f"Error {e}")
+  
